7d244737194e253fcdf354d57f0723df9f569496
Nolan Byrnes <nolanehs@gmail.com>
2017-11-27 12:12:28 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,23 +13,12 @@ class Artist
   @@artists = []
 
   def initialize
-    @@artists << self
+    super
     @songs = []
   end
 
   def self.all
     @@artists
   end
-
-
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
-
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
-
 
 end
