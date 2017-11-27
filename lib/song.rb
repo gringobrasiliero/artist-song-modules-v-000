72af1821575e5f0677859c26520e586dfbba3385
Nolan Byrnes <nolanehs@gmail.com>
2017-11-27 12:14:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,18 +11,11 @@ include Paramable
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
-
-
-
   def artist=(artist)
     @artist = artist
   end
 
-
   def self.all
     @@songs
   end
-end
+
